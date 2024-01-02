7c51ce671dba6351a6c75aaa92d7f98e741c8e31
CarrotBRRR <69835046+CarrotBRRR@users.noreply.github.com>
2024-01-01 19:32:01 -0700

    removed ctx in q.go

--- v2/main-v2.py
+++ v2/main-v2.py
@@ -557,10 +557,10 @@ async def go(ctx: comms.Context):
                     with open(filepath, 'r') as f:
                         config = json.load(f)
 
-                    channel = dc.utils.get(ctx.guild.channels, id=config["Q Channel"])
+                    channel = dc.utils.get(guild.channels, id=config["Q Channel"])
                     messages = []
 
-                    print(f'Retrieving Quotes for {ctx.guild} in {channel.name}')
+                    print(f'Retrieving Quotes for {guild} in {channel.name}')
                     
                     async for message in channel.history(limit=None):
                         messages.append(message)
