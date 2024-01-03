2708735f4a69c361a1b855c78c21c3bc3f6ef311
CarrotBRRR <69835046+CarrotBRRR@users.noreply.github.com>
2024-01-02 18:39:06 -0700

    Added ehpemeral responses to some admin commands

--- v2/main-v2.py
+++ v2/main-v2.py
@@ -477,6 +477,7 @@ async def setQChannel(ctx, qchannel: dc.TextChannel):
         channel = bot.get_channel(config["Q Channel"])
 
         print(f'Q Channel for {ctx.guild.name} is now {channel}')
+        await ctx.send(f'{channel.name} has been set to be the quotes channel!', ephemeral=True)
 
     else: 
         await ctx.send("invalid number of channels")
@@ -517,6 +518,7 @@ async def refresh(ctx):
     await updateLB(ctx)
 
     print("Leaderboard Manually Refreshed!")
+    await ctx.send(f'Leaderboard Refreshed!', ephemeral=True)
 
 
 # -------------------------------- Owner Commands ---------------------------------
