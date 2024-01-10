3056e88b09c8843db2019cf7bfe086f69ed70e19
CarrotBRRR <69835046+CarrotBRRR@users.noreply.github.com>
2024-01-09 17:38:14 -0700

    Added File Logging to bat file

--- v2/Mention-Counter-Start-V2.bat
+++ v2/Mention-Counter-Start-V2.bat
@@ -1,6 +1,9 @@
 @echo off
+set LOGFILE_TIME=%TIME:~0,2%-%TIME:~3,2%
+set LOGFILE=%DATE%_%LOGFILE_TIME%.log
 echo ----------------- Checking for Updates ----------------
 git pull origin main
 echo --------------------- Starting Up ---------------------
-python3.11.exe ./main-v2.py
+python3.11.exe ./main-v2.py >> LOGFILE
+
 call Mention-Counter-Start-V2.bat
