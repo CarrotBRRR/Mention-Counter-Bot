15355122a198b334f338d9e3aedd408602ea7a36
CarrotBRRR <69835046+CarrotBRRR@users.noreply.github.com>
2024-01-09 17:39:08 -0700

    Added File Logging to bat file

--- v2/Mention-Counter-Start-V2.bat
+++ v2/Mention-Counter-Start-V2.bat
@@ -1,6 +1,6 @@
 @echo off
 set LOGFILE_TIME=%TIME:~0,2%-%TIME:~3,2%
-set LOGFILE=%DATE%_%LOGFILE_TIME%.log
+set LOGFILE=./logs/%DATE%_%LOGFILE_TIME%.log
 echo ----------------- Checking for Updates ----------------
 git pull origin main
 echo --------------------- Starting Up ---------------------
