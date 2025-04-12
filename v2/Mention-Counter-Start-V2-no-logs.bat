2767d81ef9cf88ad0965663be90f0c6805064b01
CarrotBRRR <69835046+CarrotBRRR@users.noreply.github.com>
2025-04-11 22:16:41 -0600

    V2 No Logs .bat

---
+++ v2/Mention-Counter-Start-V2-no-logs.bat
@@ -0,0 +1,5 @@
+@echo off
+echo ----------------- Checking for Updates ----------------
+git pull origin main
+echo --------------------- Starting Up ---------------------
+call Mention-Counter-Start-V2.bat
