942f2b31eb32999a06b968ceed61df05c78590cf
CarrotBRRR <69835046+CarrotBRRR@users.noreply.github.com>
2025-04-11 22:42:39 -0600

    Fixed no-logs start

--- v2/Mention-Counter-Start-V2-no-logs.bat
+++ v2/Mention-Counter-Start-V2-no-logs.bat
@@ -2,4 +2,6 @@
 echo ----------------- Checking for Updates ----------------
 git pull origin main
 echo --------------------- Starting Up ---------------------
+python3.11.exe -u ./main-v2.py
+cls
 call Mention-Counter-Start-V2.bat
