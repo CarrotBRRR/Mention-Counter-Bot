0c712eda9f6c3c49d195dd5b03f9f6d111871d0e
CarrotBRRR <69835046+CarrotBRRR@users.noreply.github.com>
2025-06-10 05:08:37 +0000

    Upgrade to python 3.13

--- v2/Mention-Counter-Start-V2-no-logs.bat
+++ v2/Mention-Counter-Start-V2-no-logs.bat
@@ -1,7 +1,8 @@
+cls
 @echo off
 echo ----------------- Checking for Updates ----------------
 git pull origin main
 echo --------------------- Starting Up ---------------------
-python3.11.exe -u ./main-v2.py
+python3.13.exe -u ./main-v2.py
 cls
 call Mention-Counter-Start-V2.bat
